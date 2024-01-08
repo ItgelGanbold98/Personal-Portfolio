8bd28354dbeb61d568096d26495c43436f3bfcf8
Itgel Ganbold <itgel98@gmail.com>
2024-01-08 13:34:18 +0000

    Mvp done

---
+++ ChatBotServer/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import OpenAI from 'openai';
+import cors from 'cors';
+
+// Configure environment variables
+dotenv.config();
+
+// Initialize express app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// OpenAI API setup
+const OPENAI_API_KEY = process.env.OPEN_AI_API_KEY;
+const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY
+});
+
+// Rate limit middleware for chat endpoint
+const chatLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 30, // limit each IP to 30 requests per windowMs
+    standardHeaders: true,
+    legacyHeaders: false
+});
+
+// Chat endpoint
+app.post('/chat', chatLimiter, async (req, res) => {
+    const userMessage = req.body.message;
+    console.log('User message: ', userMessage);
+    try {
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: userMessage }],
+            stream: false
+        });
+
+        const botResponse = response.choices[0].message;
+        console.log('Bot reply: ', botResponse);
+        res.json({ message: botResponse });
+    } catch (error) {
+        console.error('Error in OpenAI API call:', error);
+        res.status(500).send('An error occurred in generating a response');
+    }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,203 @@
+import React, { useState, useRef, useEffect, useContext } from 'react';
+import styled from 'styled-components';
+import { FaArrowCircleUp } from 'react-icons/fa';
+import axios from 'axios';
+import { ThemeContext } from '../../context';
+
+const Chat = () => {
+    const theme = useContext(ThemeContext);
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState('');
+    const messagesEndRef = useRef(null);
+
+    useEffect(() => {
+        console.log('Theme ', theme);
+    }, [theme]);
+
+    const sendMessageToServer = async (userInput) => {
+        try {
+            console.log('User message: ', userInput);
+            const response = await axios.post('http://localhost:3000/chat', {
+                message: userInput
+            });
+            console.log('Reply: ', response.data.message.content);
+            return response.data.message.content;
+        } catch (error) {
+            console.error('Error sending message to server:', error);
+            return 'Looks like my assistant is asleep 😴';
+        }
+    };
+
+    const MAX_MESSAGES_PER_SESSION = 10;
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        if (!input.trim()) return;
+
+        // Check if the message limit has been reached
+        if (messages.length >= MAX_MESSAGES_PER_SESSION * 2) {
+            alert('Sorry, only 10 messages allowed per session! 👮');
+            return;
+        }
+
+        // Add user message to chat
+        const newUserMessage = { role: 'user', content: input };
+        setMessages([...messages, newUserMessage]);
+
+        // Send message to server and get response
+        const botResponse = await sendMessageToServer(input);
+
+        // Add bot response to chat
+        const newBotMessage = { role: 'bot', content: botResponse };
+        setMessages((prevMessages) => [...prevMessages, newBotMessage]);
+
+        setInput('');
+        inputRef.current.focus();
+    };
+
+    //For auto scrolling to the latest message
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages]);
+
+    const inputRef = useRef(null);
+
+    return (
+        <Container
+            style={{
+                backgroundColor: theme.state.darkMode ? '#2b303d' : 'white',
+                borderRadius: '10px'
+            }}
+        >
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '300px',
+                    overflow: 'auto',
+                    marginBottom: '10px',
+                    padding: '10px',
+                    backgroundColor: theme.state.darkMode && '#2b303d',
+                    borderRadius: '10px'
+                }}
+            >
+                {messages.map((message, index) =>
+                    message.role === 'user' ? (
+                        <UserMessageContainer key={index}>
+                            <UserMessage>{message.content}</UserMessage>
+                        </UserMessageContainer>
+                    ) : (
+                        <BotMessageContainer key={index}>
+                            <BotMessage>{message.content}</BotMessage>
+                        </BotMessageContainer>
+                    )
+                )}
+                <div ref={messagesEndRef} />{' '}
+                {/* Invisible element at the end of messages */}
+            </div>
+            <Form
+                onSubmit={sendMessage}
+                style={{ backgroundColor: theme.state.darkMode && '#282f40' }}
+            >
+                <input
+                    ref={inputRef}
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="Ask about me!"
+                    style={{
+                        marginRight: '10px',
+                        padding: '10px',
+                        width: '200px',
+                        height: '30px',
+                        backgroundColor: 'white',
+                        borderRadius: '5px',
+                        backgroundColor: theme.state.darkMode && '#242a3a',
+                        color: theme.state.darkMode ? 'white' : 'black'
+                    }}
+                />
+                <Button type="submit">
+                    <FaArrowCircleUp
+                        style={{
+                            height: '50px',
+                            width: '45px',
+                            color: theme.state.darkMode ? 'black' : 'navy'
+                        }}
+                    />
+                </Button>
+            </Form>
+        </Container>
+    );
+};
+
+export default Chat;
+
+const Container = styled.div`
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    width: 300px;
+    z-index: 1000;
+    width: 330px;
+    color: black;
+    padding: 0px;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: aliceblue;
+    border-radius: 10px;
+`;
+
+const Button = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    padding: 0;
+    background-color: white;
+`;
+
+const UserMessageContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    height: auto;
+    margin-bottom: 5px;
+`;
+
+const UserMessage = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    overflow-wrap: break-word;
+    word-break: break-word; /* This ensures words break to prevent overflow */
+    background-color: aliceblue;
+    width: 150px;
+    height: auto;
+    padding: 5px; /* Add some padding for better readability */
+    border-radius: 10px; /* Optional: for rounded corners */
+`;
+
+const BotMessageContainer = styled.div`
+    display: flex;
+    justify-content: flex-start; // Align bot messages to the left
+    width: 100%;
+    margin-bottom: 5px;
+`;
+
+const BotMessage = styled.div`
+    display: flex;
+    align-items: center;
+    background-color: #fff0ff; // Different background for bot messages
+    width: 150px;
+    padding: 5px;
+    border-radius: 10px;
+`;
